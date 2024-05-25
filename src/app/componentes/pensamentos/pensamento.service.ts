7a105eaf7e84b20e98a75c69d789422178e601fb
wander.franca <wander.franca@grupotrigo.com.br>
2024-05-25 19:51:38 -0300

    Correcoes no backend

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pensamento } from './pensamento/pensamento';
 import { Observable } from 'rxjs';
